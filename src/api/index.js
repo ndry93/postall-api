515d08d49a4f327c569188a16b12b468bf3b3469
Hendry Yuwono <ndry93@yahoo.co.id>
2020-05-27 21:58:41 +0700

    remove unnecessary

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,7 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-const fs = require('fs');
-
-const path = require('path');
 
 const Router = require('koa-joi-router');
-
-const baseName = path.basename(__filename);
 
 const docs = require('../docs');
 
@@ -21,17 +16,6 @@ function applyApiMiddleware(app) {
 
 	// Docs routes
 	let swaggerRoutes = [];
-	// fs.readdirSync(__dirname)
-	// 	.filter(file => {
-	// 		return file.indexOf('.') !== 0 && file.indexOf('_') !== 0 && file !== baseName;
-	// 	})
-	// 	.forEach(file => {
-	// 		const api = require(path.join(__dirname, file))(Router);
-	// 		mainRouter.use(api.middleware());
-	// 		// Docs generation
-	// 		const swaggerSpec = docs.addDocsForRouter(api).generateSpec();
-	// 		swaggerRoutes = [...docs.getRoutesForSpec(swaggerSpec)];
-	// 	});
 
 	mainRouter.use(poster.middleware());
 	swaggerRoutes = [...docs.getRoutesForSpec(docs.addDocsForRouter(poster).generateSpec())];
