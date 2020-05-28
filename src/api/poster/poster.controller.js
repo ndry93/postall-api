785b2c180c2ca1495d2749168a894352378d2493
Hendry Yuwono <ndry93@yahoo.co.id>
2020-05-29 00:10:08 +0700

    correct ctx.response.status code

--- src/api/poster/poster.controller.js
+++ src/api/poster/poster.controller.js
@@ -2,20 +2,32 @@ const { logger } = require('../../utils/logger')(__filename);
 
 module.exports = services => {
 	async function showRequest(ctx) {
+		const { request, response } = ctx;
 		logger.child({ uid: ctx.params.uid });
 		logger.info('uid ', ctx.params.uid);
 		logger.info('showRequest ', ctx.request);
 		const isValid = services.posterService.checkValidUID(ctx.params.uid);
 		logger.info('is uid valid ', isValid);
+		if (!isValid) {
+			response.status = 404;
+			ctx.throw(404, 'uid is invalid or expired');
+		}
 
-		const { request, response } = ctx;
-
-		const httpReqObj = { request, body: request.body };
-		const httpResObj = { response, body: response.body };
+		response.status = 200;
 
 		const responseData = {
-			request: httpReqObj,
-			response: httpResObj
+			request: {
+				method: request.method,
+				header: request.header,
+				url: request.url,
+				body: request.body
+			},
+			response: {
+				status: response.status,
+				message: response.message,
+				header: response.header,
+				body: response.body
+			}
 		};
 
 		logger.info('responseData ', responseData);
